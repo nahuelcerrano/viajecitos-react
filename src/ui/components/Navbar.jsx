b25bb07570bcb5fc3892873963becb5dbffda313
nahuelcerrano <nahuelcerrano@gmail.com>
2023-02-02 23:48:16 -0300

    Navbar con propiedad fixed

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import navStyles from "./navbar.module.css";
 
 export const Navbar = () => {
+  
+  const [fixed, setFixed] = useState(false)
+
+  const navbar = useRef()
+  const navbarOffset = useRef()
+
+  useEffect(() => {
+    
+    const handleScroll = () => {
+      const value = navbarOffset.current.clientHeight
+
+      setFixed(window.pageYOffset >= value)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => 
+      window.removeEventListener('scroll', handleScroll)
+  },[])
+
   return (
-    <div className={navStyles.header}>
-      <nav className={`${navStyles.navbar} row`}>
+    <div 
+      className={`${navStyles.header}  ${fixed && navStyles.fixed} ${!fixed && 'animate__animated animate__fadeIn animate__faster'}`}
+      ref={navbarOffset}>
+      <nav
+      // ${fixed && navStyles.fixed} ${fixed && 'animate__animated animate__slideOutUp'}
+      className={`${navStyles.navbar} row`}
+      ref={navbar}>
         <Link className={navStyles.logo} to="/">
           Viajecitos
         </Link>
@@ -25,7 +50,7 @@ export const Navbar = () => {
         </ul>
 
         <Link to="/contact">
-          <button className={navStyles.contacto}>
+          <button className={`${navStyles.contacto} ${fixed && navStyles.contactAfter}`}>
             Contacto
           </button>
         </Link>
