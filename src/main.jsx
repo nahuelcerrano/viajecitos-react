794188185a1a09b430427e238be80e0355b0ccde
nahuelcerrano <nahuelcerrano@gmail.com>
2024-02-20 16:42:31 -0300

    Se agrego el helper scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { ViajecitosApp } from './ViajecitosApp'
+import { ScrollToTop } from './helpers/ScrollToTop'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <ViajecitosApp />
     </BrowserRouter>
   </React.StrictMode>,
